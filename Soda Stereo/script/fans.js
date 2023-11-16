7f6e12d1e195cb588d24c234c50afd889ab5e932
Dante_dev <109294689+Dantlu@users.noreply.github.com>
2023-11-16 00:32:53 -0300

    Update fans.js
    form

--- Soda Stereo/script/fans.js
+++ Soda Stereo/script/fans.js
@@ -1,11 +1,19 @@
-var input = document.getElementsByClassName('formulario__input');
 
-for (var i = 0; i < input.length; i++){
-    input[i].addEventListener('keyup', function(){
-        if(this.value.length>=1){
-            this.nextElementSibling.classlist.add('fijar');
-            }else{
-            this.nextElementSibling.classlist.remove('fijar');
-        }
-    });
+const inputs = document.querySelectorAll(".formulario__input");
+const formulario = document.getElementById("form");
+
+function activarFormLabel(){
+    inputs.forEach(input =>{
+        input.addEventListener("focus",()=>{
+            const label = document.querySelector(`label[for=${input.id}]`);
+            label.classList.add("formulario__label_active");
+        });
+
+        input.addEventListener("blur",()=>{
+            if(input.value.trim()==""){
+            const label = document.querySelector(`label[for=${input.id}]`);
+            label.classList.remove("formulario__label_active");
+            }
+        })
+    })
 }
